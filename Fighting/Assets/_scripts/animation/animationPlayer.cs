5614f37aedd4a370e307f188e2e5f75709ba8efe
zhenghua <ctzhenghua@163.com>
2018-10-12 17:28:34 +0800

    Update Animation

---
+++ Fighting/Assets/_scripts/animation/animationPlayer.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public enum ANIMATION_TYPE{
+    RUN,
+    STAY,
+    ATTACK,
+    DEAD
+}
+public class animationPlayer
+{
+    [SerializeField]
+    private static Animator m_AnimationController;
+
+    private static animationPlayer m_Instance = null;
+    private animationPlayer()
+    {
+        m_AnimationController = GameObject.Find("Player_Chan").GetComponent<Animator> ();
+    }
+
+    public static animationPlayer getInstance()
+    {
+        if (m_Instance == null)
+            m_Instance = new animationPlayer();
+
+        return m_Instance;
+    }
+
+    public void SetCondition(ANIMATION_TYPE type)
+    {
+        switch(type)
+        {
+            case ANIMATION_TYPE.RUN:
+                    m_AnimationController.SetBool("Run", true);
+                break;
+            case ANIMATION_TYPE.STAY:
+                    m_AnimationController.SetBool("Run", false);
+                break;
+            case ANIMATION_TYPE.ATTACK:
+                     m_AnimationController.SetBool("Spinkick", true);
+                break;
+            case ANIMATION_TYPE.DEAD:
+                break;
+        }
+    }
+}
