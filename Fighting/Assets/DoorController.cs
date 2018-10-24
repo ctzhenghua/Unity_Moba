5aa59cd54984b0e58dc18ecdb09d1a0cac045a74
andlightlight <327762932@qq.com>
2018-10-24 15:15:13 +0800

    去掉library等临时生成的东西，清理工程文件

---
+++ Fighting/Assets/DoorController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour {
+    [SerializeField]
+    private Animation anim;
+
+    private bool isOpen = false;
+    private bool isClose = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!isOpen)
+        {
+            isOpen = true;
+            isClose = false;
+            anim.Play("openDoor");
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (!isClose)
+        {
+            isClose = true;
+            isOpen = false;
+            anim.Play("closeDoor");
+        }
+    }
+}
