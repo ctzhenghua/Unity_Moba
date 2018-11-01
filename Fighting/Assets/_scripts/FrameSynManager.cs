da0fcf4580a65d331d9fe0d26c83414fc284de9e
andlightlight <327762932@qq.com>
2018-11-01 20:41:41 +0800

    帧同步，先占个坑位

---
+++ Fighting/Assets/_scripts/FrameSynManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+
+public class FrameSynManager
+{
+	public abstract class IFrameObjS
+	{
+
+	}
+
+	public abstract class IFrameObjC
+	{
+
+	}
+
+	public abstract class IFrameSynAgent
+	{
+
+	}
+
+	private int m_CurrentFrameIndex = 0;
+	private Dictionary<int, IFrameObjS> m_FrameObjSDic;
+	private List<IFrameSynAgent> m_FrameSynAgentList;
+	private bool m_IsStart = false;
+	private List<IFrameSynAgent> m_ControlFrameObjCDList;
+	private int m_ForecastFrameNum = 0;
+	private Dictionary<int, IFrameObjS> m_ForecastFrameObjSDic;
+	private long m_LastFrameTime = 0;
+
+	public void HandleFrame(int num)
+	{
+
+	}
+
+	public void RegisterAgent(IFrameSynAgent agent)
+	{
+
+	}
+
+	public void RemoveAgent(IFrameSynAgent agent)
+	{
+
+	}
+
+	public void AddFrame(List<IFrameObjS> frameList)
+	{
+
+	}
+}
